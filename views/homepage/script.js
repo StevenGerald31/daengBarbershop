866b736895ac950862815dfd17ef5fae6aa3db5b
reinrow <reindrowajja@gmail.com>
2024-03-04 18:59:18 +0700

    halo

--- views/homepage/script.js
+++ views/homepage/script.js
@@ -4,3 +4,12 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+// Klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (event) {
+  if (!hamburger.contains(event.target) && !navbarNav.contains(event.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
