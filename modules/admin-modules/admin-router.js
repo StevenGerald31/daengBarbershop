f3899074bc2f2992a0fe8d4779d758cdf79dd000
reinrow <reindrowajja@gmail.com>
2024-08-12 09:54:08 +0700

    ping

--- modules/admin-modules/admin-router.js
+++ modules/admin-modules/admin-router.js
@@ -7,5 +7,6 @@ router.get("/pelanggan", adminController.data_pelanggan);
 router.get("/server", adminController.data_server);
 router.get("/lokasi", adminController.data_lokasi);
 router.post("/tambahServer", adminController.tambah_server);
+router.post("/resetPassword", adminController.resetPassword);
 
 module.exports = router;
